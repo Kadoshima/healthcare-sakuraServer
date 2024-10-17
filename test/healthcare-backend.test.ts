e9059485390ffca0055d47070475d3ce53368575
Kadoshima <galaxykadosima@gmail.com>
2024-10-17 16:54:14 +0900

    Initial commit

---
+++ test/healthcare-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HealthcareBackend from '../lib/healthcare-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/healthcare-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HealthcareBackend.HealthcareBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
